0e94521c4fdb65d3dcc5b23114ea981883c15f16
Michael Wolfe <mswolfe@gmail.com>
2016-09-04 21:39:12 -0700

    Adding a new sub-project to show example useage

---
+++ spring-query-filter-examples/src/main/java/org/wolfe/SpringQueryFilterExamplesApplication.java
@@ -0,0 +1,12 @@
+package org.wolfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueryFilterExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQueryFilterExamplesApplication.class, args);
+	}
+}
