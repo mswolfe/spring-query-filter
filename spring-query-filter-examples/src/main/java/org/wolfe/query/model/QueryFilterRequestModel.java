160b27a68638924451ecafcfa26be5a3a431420d
Michael Wolfe <mswolfe@gmail.com>
2016-09-16 23:00:36 -0700

    Initial version of integration tests

---
+++ spring-query-filter-examples/src/main/java/org/wolfe/query/model/QueryFilterRequestModel.java
@@ -0,0 +1,53 @@
+package org.wolfe.query.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.wolfe.query.QueryParamOperator;
+
+import javax.validation.constraints.NotNull;
+
+public class QueryFilterRequestModel {
+
+    @NotNull
+    private String id;
+
+    @NotEmpty
+    private String email;
+
+    private float balance;
+
+    @QueryParamOperator(allowed = {">", "<"})
+    private String balanceOperator;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getBalanceOperator() {
+        return balanceOperator;
+    }
+
+    public void setBalanceOperator(String balanceOperator) {
+        this.balanceOperator = balanceOperator;
+    }
+}
---
+++ spring-query-filter-examples/src/test/groovy/org/wolfe/query/web/QueryFilterControllerSpec.groovy
@@ -0,0 +1,112 @@
+package org.wolfe.query.web
+
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import org.apache.http.HttpStatus
+import org.springframework.beans.factory.annotation.Autowire
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.context.WebApplicationContext
+import org.wolfe.Application
+import spock.lang.Specification
+
+@ContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application)
+class QueryFilterControllerSpec extends Specification {
+
+    @Value('${local.server.port}')
+    int port
+
+    HTTPBuilder http
+
+    @Autowired
+    WebApplicationContext context
+
+    def setup() {
+        http = new HTTPBuilder("http://localhost:$port")
+    }
+
+    def "should boot up with no errors"() {
+        expect: "web application context exists"
+        context != null
+    }
+
+    def "standardQuery should return id"() {
+        given:
+        def filter = "'id=12345'"
+
+        when:
+        def (status, data) = http.get(path: "/query", query: [filter:filter]) {
+            resp, reader -> [resp.status, reader]
+        }
+
+        then:
+        status == HttpStatus.SC_OK
+        data.id == "12345"
+    }
+
+    def "standardQuery should return all values"() {
+        given:
+        def filter = "'id=12345&email=bob@aol.com&balance>50'"
+
+        when:
+        def (status, data) = http.get(path: "/query", query: [filter:filter]) {
+            resp, reader -> [resp.status, reader]
+        }
+
+        then:
+        status == HttpStatus.SC_OK
+        data.id == "12345"
+        data.email == "bob@aol.com"
+        data.balance == 50.0f
+        data.balanceOperator == ">"
+    }
+
+    def "standardQueryWithValidation should return a 400 Bad Request when id is null"() {
+        given:
+        def filter = "'email=bob@aol.com&balance>50'"
+
+        when:
+        int status
+        http.handler.failure = {
+            resp -> status = resp.status
+        }
+        http.get(path: "/validate", query: [filter:filter])
+
+        then:
+        status == HttpStatus.SC_BAD_REQUEST
+    }
+
+    def "standardQueryWithValidation should return a 400 Bad Request when email is blank"() {
+        given:
+        def filter = "'id=12345&email=&balance>50'"
+
+        when:
+        int status
+        http.handler.failure = {
+            resp -> status = resp.status
+        }
+        http.get(path: "/validate", query: [filter:filter])
+
+        then:
+        status == HttpStatus.SC_BAD_REQUEST
+    }
+
+    def "standardQueryWithValidation should return a 400 Bad Request when balance operator is invalid"() {
+        given:
+        def filter = "'id=12345&email=bob@aol.com&balance=50'"
+
+        when:
+        int status
+        http.handler.failure = {
+            resp -> status = resp.status
+        }
+        http.get(path: "/validate", query: [filter:filter])
+
+        then:
+        status == HttpStatus.SC_BAD_REQUEST
+    }
+
+}
