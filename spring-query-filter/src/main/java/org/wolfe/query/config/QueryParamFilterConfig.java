88a679d959582fbd5955aa564f99228dbcafb584
Michael Wolfe <mswolfe@gmail.com>
2016-09-22 17:05:43 -0700

    Adding auto-configuration
     Conflicts:
    	spring-query-filter/build.gradle

--- spring-query-filter/src/main/java/org/wolfe/query/config/QueryParamFilterConfig.java
+++ spring-query-filter/src/main/java/org/wolfe/query/config/QueryParamFilterConfig.java
@@ -1,14 +1,23 @@
 package org.wolfe.query.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.validation.Validator;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.wolfe.query.pattern.DefaultQueryFilterPatternProvider;
+import org.wolfe.query.pattern.QueryFilterPatternProvider;
 import org.wolfe.query.support.QueryFilterHandlerMethodArgumentResolver;
 
 import java.util.List;
 
 @Configuration
+@AutoConfigureAfter(WebMvcAutoConfiguration.class)
 public class QueryParamFilterConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
@@ -17,5 +26,11 @@ public class QueryParamFilterConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(queryFilterHandlerMethodArgumentResolver);
+    }
+
+    @Bean
+    public QueryFilterHandlerMethodArgumentResolver getQueryFilterHandlerMethodArgumentResolver(ConversionService conversionService,
+                                                                                                @Qualifier("mvcValidator") Validator validator) {
+        return new QueryFilterHandlerMethodArgumentResolver(new DefaultQueryFilterPatternProvider(), conversionService, validator);
     }
 }
